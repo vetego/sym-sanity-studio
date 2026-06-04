447d7d92f289f4c39fb82996f2eb1e5f2b0fda61
Vladimir Egorov <vetego@gmail.com>
2026-06-04 16:09:33 +0200

    Legal pages cleanup

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,12 +2,10 @@ import post from './post'
 import event from './event'
 import eventHost from './eventHost'
 import page from './page'
-import legalPage from './legalPage'
 
 export const schemaTypes = [
   eventHost,
   post,
   event,
   page,
-  legalPage,
 ]
